4d9078449b541f7b1cee7f601b5a39f50ba18c0c
Bryan Galindo <galindo.bryan08@gmail.com>
2021-08-06 06:56:44 -0500

    Returning more concise Yelp URL to return a readable URL.

--- get_yelp_data.py
+++ get_yelp_data.py
@@ -16,7 +16,17 @@ def convert_json_to_dict(json_file:str) -> dict:
         return json.load(json_file)
     
 def filter_yelp_response(response:dict) -> dict:
-    pass
+    if len(response["businesses"]) != 0:
+        restaurant_node = response["businesses"][0]
+        return {
+            "yelp_id": restaurant_node["id"],
+            "yelp_name": restaurant_node["name"], 
+            "review_count": restaurant_node["review_count"], 
+            "rating": restaurant_node["rating"], 
+            "caategories": restaurant_node["categories"], 
+            "phone": restaurant_node["phone"],
+            "yelp_url": restaurant_node["url"].split('?')[0],
+        }
 
 def get_yelp_data(restaurant:dict) -> dict:
     response = requests.get(
@@ -26,7 +36,7 @@ def get_yelp_data(restaurant:dict) -> dict:
             restaurant['coordinates']['longitude'],
             ),
         headers=headers)
-    return response.json()
+    return filter_yelp_response(response.json())
 
 def update_restaurant_json_file(yelp_data:dict) -> None:
     pass
@@ -39,3 +49,12 @@ def is_similar(restaurant_name:dict) -> bool:
 
 if __name__ =='__main__':
     restaurants_dict = convert_json_to_dict("restaurant_data.json")
+    restaurant = {
+            "name": "Acadian Coast",
+            "hrw_url": "https://houstonrestaurantweeks.com/restaurants/acadian-coast/",
+            "coordinates": {
+                "latitude": "29.7564351",
+                "longitude": "-95.3401952"
+            }
+        }
+    print(get_yelp_data(restaurant))
