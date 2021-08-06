bce2ea662f082105a9f06f3a1ebfa1113a180056
Bryan Galindo <galindo.bryan08@gmail.com>
2021-08-06 00:57:10 -0500

    Finishing json to dict converter function.

--- get_yelp_data.py
+++ get_yelp_data.py
@@ -1,13 +1,14 @@
 import os
-
+import json
 
 API_TOKEN = os.getenv('API_TOKEN')
 headers = {
     "Authorization": f"Bearer {API_TOKEN}",
 }
 
-def get_restaurant_data_dict(json_file:str) -> dict:
-    pass
+def convert_json_to_dict(json_file:str) -> dict:
+    with open(json_file, encoding='utf-8') as json_file:
+        return json.load(json_file)
 
 def get_yelp_data(restaurant:dict) -> dict:
     pass
@@ -22,5 +23,5 @@ def is_similar(restaurant_name:dict) -> bool:
     pass
 
 if __name__ =='__main__':
-    restaurants_dict = get_restaurant_data_dict("restaurant_data.json")
+    restaurants_dict = convert_json_to_dict("restaurant_data.json")
     update_restaurants_with_yelp_data()
