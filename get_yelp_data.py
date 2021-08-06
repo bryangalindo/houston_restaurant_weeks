5f2d236b6301ad1bda2abf1caeb3197ef92c2606
Bryan Galindo <galindo.bryan08@gmail.com>
2021-08-06 07:33:33 -0500

    Flattening headers to improve readability

--- get_yelp_data.py
+++ get_yelp_data.py
@@ -8,9 +8,7 @@ import requests
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 API_KEY = os.getenv('API_KEY')
-headers = {
-    "Authorization": f"Bearer {API_KEY}",
-}
+headers = {"Authorization": f"Bearer {API_KEY}"}
 
 yelp_api_endpoint = 'https://api.yelp.com/v3/businesses/search?term={}&latitude={}&longitude={}'
 
@@ -30,10 +28,10 @@ def filter_yelp_response(response:dict) -> dict:
             "phone": restaurant_node["phone"],
             "yelp_url": restaurant_node["url"].split('?')[0],
         }
-        logging.info(f'Filtered Yelp response. Result: {filter_yelp_response}')
+        logging.info(f'Filtered Yelp response. Result: {filtered_yelp_response}.')
         return filtered_yelp_response
     else:
-        logging.warning(f'Yelp Search API did not return businesses')
+        logging.warning(f'Yelp Search API did not return any businesses.')
 
 def get_yelp_data(restaurant:dict) -> dict:
     url = yelp_api_endpoint.format(
@@ -48,12 +46,12 @@ def get_yelp_data(restaurant:dict) -> dict:
         filtered_dict = filter_yelp_response(response.json())
         return filtered_dict
     else:
-        logging.warning(f'Unsuccessful call to Yelp API. Status code: {response.status_code}')
+        logging.warning(f'Unsuccessful call to Yelp API. Status code: {response.status_code}. {response.reason}.')
 
 def update_restaurant_json_file(yelp_data:dict) -> None:
     pass
 
-def update_restaurants_with_yelp_data(restaurants:list) -> None:
+def update_all_restaurants_with_yelp_data(restaurants:list) -> None:
     pass
 
 def is_similar(restaurant_name:dict) -> bool:
@@ -61,12 +59,3 @@ def is_similar(restaurant_name:dict) -> bool:
 
 if __name__ =='__main__':
     restaurants_dict = convert_json_to_dict("restaurant_data.json")
-    restaurant = {
-            "name": "Acadian Coast",
-            "hrw_url": "https://houstonrestaurantweeks.com/restaurants/acadian-coast/",
-            "coordinates": {
-                "latitude": "29.7564351",
-                "longitude": "-95.3401952"
-            }
-        }
-    print(get_yelp_data(restaurant))
